2776aba0e0e2809c7a097b192bde9420aab59641
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-04-29 21:25:04 +0200

    Fix google search
    Url encode where necessary

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -357,13 +357,62 @@ xmap < ]
 
 vim.cmd([[
 function! JiraSearch()
-     let searchterm = getreg("g")
-     silent! exec "silent! !google-chrome \"https://eteamproject.internal.ericsson.com/browse/" . searchterm . "\" &"
+     silent! exec "silent! !google-chrome \"https://eteamproject.internal.ericsson.com/browse/" . @j . "\" &"
 endfunction
-vnoremap <leader>fj "gy<Esc>:call JiraSearch()<CR>
+vnoremap <leader>fj "jy<Esc>:call JiraSearch()<CR>
+function! UrlEncode(string)
+
+    let result = ""
+
+    let characters = split(a:string, '.\zs')
+    for character in characters
+        if character == " "
+            let result = result . "+"
+        elseif CharacterRequiresUrlEncoding(character)
+            let i = 0
+            while i < strlen(character)
+                let byte = strpart(character, i, 1)
+                let decimal = char2nr(byte)
+                let result = result . "%" . printf("%02x", decimal)
+                let i += 1
+            endwhile
+        else
+            let result = result . character
+        endif
+    endfor
+
+    return result
+
+endfunction
+
+" Returns 1 if the given character should be percent-encoded in a URL encoded
+" string.
+function! CharacterRequiresUrlEncoding(character)
+
+    let ascii_code = char2nr(a:character)
+    if ascii_code >= 48 && ascii_code <= 57
+        return 0
+    elseif ascii_code >= 65 && ascii_code <= 90
+        return 0
+    elseif ascii_code >= 97 && ascii_code <= 122
+        return 0
+    elseif a:character == "-" || a:character == "_" || a:character == "." || a:character == "~"
+        return 0
+    endif
+
+    return 1
+
+endfunction
+
 function! GoogleSearch()
-     let searchterm = getreg("g")
-     silent! exec "silent! !google-chrome \"http://google.com/search?q=" . searchterm . "\" &"
+   let searchterm = getreg("g")
+   let searchterm = substitute(searchterm, "\n", " ", "g")
+   let searchterm = UrlEncode(searchterm)
+   let searchterm = shellescape(searchterm, 1)
+   silent! exec "!google-chrome 'https://google.com/search?q=" . searchterm . "'"
+   redraw!
 endfunction
 vnoremap <leader>fg "gy<Esc>:call GoogleSearch()<CR>
+" Added this to google the word under the cursor
+nnoremap <leader>fgw b"gye:call GoogleSearch()<CR>
 ]])
