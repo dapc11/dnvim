44d9333dd5bbcc9e1c2c17b2a048eea0f3748994
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-05-01 22:26:45 +0200

    Starter tweaks

--- lua/configs/mini/init.lua
+++ lua/configs/mini/init.lua
@@ -27,16 +27,15 @@ function M.config()
 	starter.setup({
 		evaluate_single = true,
 		items = {
-			starter.sections.telescope(),
-			starter.sections.recent_files(5, false),
-			starter.sections.recent_files(10, true),
+			starter.sections.recent_files(15),
 			-- Use this if you set up 'mini.sessions'
 			starter.sections.sessions(5, true),
+			starter.sections.telescope(),
 			starter.sections.builtin_actions(),
 		},
 		content_hooks = {
 			starter.gen_hook.adding_bullet(),
-			starter.gen_hook.indexing("all", { "Builtin actions" }),
+			starter.gen_hook.indexing("all", { "Builtin actions", "Telescope" }),
 			starter.gen_hook.padding(3, 2),
 		},
 	})
