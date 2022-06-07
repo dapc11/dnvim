100504a22860432e0ba729b782a0db362a7a7018
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-06-07 12:47:39 +0200

    Fix quickfix navigation

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -229,8 +229,8 @@ map("n", "ga", ":EasyAlign<CR>")
 map("v", "<Tab>", ">gv")
 map("v", "<S-Tab>", "<gv")
 map("i", "<S-Tab>", "<C-d>")
-map("n", "[g", ":cnext<CR>")
-map("n", "]g", ":cprevious<CR>")
+map("n", "]g", ":cnext<CR>")
+map("n", "[g", ":cprevious<CR>")
 
 vim.cmd([[
 function! JiraSearch()
