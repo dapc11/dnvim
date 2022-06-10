41663b0b4f9bcaed0f68d2205848b85469399f40
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-06-10 23:31:40 +0200

    Setup dapui and personal tweaks

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -233,12 +233,6 @@ packer.startup({
         require("configs.surround").config()
       end,
     })
-    -- use({
-    --   "dapc11/shade.nvim",
-    --   config = function()
-    --     require("configs.shade").config()
-    --   end,
-    -- })
 
     use({
       "mfussenegger/nvim-dap",
@@ -247,7 +241,6 @@ packer.startup({
       config = function()
         require("configs.dap").setup()
         local dap, dapui = require("dap"), require("dapui")
-        dapui.setup()
         dap.listeners.after.event_initialized["dapui_config"] = function()
           dapui.open()
         end
@@ -262,6 +255,55 @@ packer.startup({
 
     use({
       "rcarriga/nvim-dap-ui",
+      config = function()
+        require("dapui").setup({
+          icons = { expanded = "▾", collapsed = "▸" },
+          mappings = {
+            -- Use a table to apply multiple mappings
+            expand = { "<CR>", "<2-LeftMouse>" },
+            open = "o",
+            remove = "d",
+            edit = "e",
+            repl = "r",
+            toggle = "t",
+          },
+          -- Expand lines larger than the window
+          -- Requires >= 0.7
+          expand_lines = vim.fn.has("nvim-0.7"),
+          sidebar = {
+            -- You can change the order of elements in the sidebar
+            elements = {
+              -- Provide as ID strings or tables with "id" and "size" keys
+              {
+                id = "scopes",
+                size = 0.50, -- Can be float or integer > 1
+              },
+              { id = "breakpoints", size = 0.25 },
+              { id = "stacks", size = 0.15 },
+              { id = "watches", size = 0.10 },
+            },
+            size = 60,
+            position = "left", -- Can be "left", "right", "top", "bottom"
+          },
+          tray = {
+            elements = { "repl" },
+            size = 10,
+            position = "bottom", -- Can be "left", "right", "top", "bottom"
+          },
+          floating = {
+            max_height = nil, -- These can be integers or a float between 0 and 1.
+            max_width = nil, -- Floats will be treated as percentage of your screen.
+            border = "single", -- Border style. Can be "single", "double" or "rounded"
+            mappings = {
+              close = { "q", "<Esc>" },
+            },
+          },
+          windows = { indent = 1 },
+          render = {
+            max_type_length = nil, -- Can be integer or nil.
+          },
+        })
+      end,
       requires = { "mfussenegger/nvim-dap" },
     })
 
