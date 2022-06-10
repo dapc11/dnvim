8247b4ddd10433e54ff193273dd7578fb5cf514d
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-06-10 07:21:54 +0200

    Disable diagnostics in neo-tree

--- lua/configs/neo_tree/init.lua
+++ lua/configs/neo_tree/init.lua
@@ -11,7 +11,7 @@ function M.config()
     close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
     popup_border_style = "single",
     enable_git_status = true,
-    enable_diagnostics = true,
+    enable_diagnostics = false,
     default_component_configs = {
       container = {
         enable_character_fade = false,
