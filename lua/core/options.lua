8bb3e3a0763737c5cb8c16cabf68df4b54e6e58b
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-06-15 22:11:19 +0200

    Markdown config

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -55,6 +55,33 @@ set background=dark
 colorscheme onedark
 ]])
 
+-- Use proper syntax highlighting in code blocks
+local fences = {
+  "lua",
+  -- "vim",
+  "json",
+  "typescript",
+  "javascript",
+  "js=javascript",
+  "ts=typescript",
+  "shell=sh",
+  "python",
+  "sh",
+  "console=sh",
+}
+g.markdown_fenced_languages = fences
+
+-- plasticboy/vim-markdown
+g.vim_markdown_folding_level = 10
+g.vim_markdown_fenced_languages = fences
+g.vim_markdown_folding_style_pythonic = 1
+g.vim_markdown_conceal_code_blocks = 0
+g.vim_markdown_frontmatter = 1
+g.vim_markdown_strikethrough = 1
+
+vim.cmd([[autocmd FileType markdown nnoremap gO <cmd>Toc<cr>]])
+vim.cmd([[autocmd FileType markdown setlocal spell]])
+
 -- Bracketed paste
 -- Code from:
 -- http://stackoverflow.com/questions/5585129/pasting-code-into-terminal-window-into-vim-on-mac-os-x
