a3e1406bd3ecad514c0ccfe8d1a05484870caee6
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-08-02 10:50:15 +0200

    Removed deprecated config for gitsigns

--- lua/configs/gitsigns/init.lua
+++ lua/configs/gitsigns/init.lua
@@ -2,8 +2,16 @@ local M = {}
 
 function M.config()
   require("gitsigns").setup({
-    keymaps = {
-      noremap = false,
+    on_attach = function(bufnr)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "hs", '<cmd>lua require"gitsigns".stage_hunk()<CR>', {})
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "hb", '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>', {})
+    end,
+    signs = {
+      add = { hl = "GitSignsAdd", text = "┃", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+      change = { hl = "GitSignsChange", text = "┃", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+      delete = { hl = "GitSignsDelete", text = "▁", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+      topdelete = { hl = "GitSignsDelete", text = "▔", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+      changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
     },
   })
 end
