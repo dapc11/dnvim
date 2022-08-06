fdcd9a5fbb19286ae534940a8518127005c58f03
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-08-06 19:34:37 +0200

    Add keymap for preview git hunk

--- lua/configs/gitsigns/init.lua
+++ lua/configs/gitsigns/init.lua
@@ -5,6 +5,7 @@ function M.config()
     on_attach = function(bufnr)
       vim.api.nvim_buf_set_keymap(bufnr, "n", "hs", '<cmd>lua require"gitsigns".stage_hunk()<CR>', {})
       vim.api.nvim_buf_set_keymap(bufnr, "n", "hb", '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>', {})
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "hp", '<cmd>lua require"gitsigns".preview_hunk()<CR>', {})
     end,
     signs = {
       add = { hl = "GitSignsAdd", text = "┃", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
