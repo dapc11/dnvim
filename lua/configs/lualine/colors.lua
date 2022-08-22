34e0b261b5d0a71c30f4be4ded5e6014a0c8b96d
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-08-22 19:54:01 +0200

    Align lualine colors with catpuccin

--- lua/configs/lualine/colors.lua
+++ lua/configs/lualine/colors.lua
@@ -1,16 +1,16 @@
 local colors = {
   bg = "#202328",
-  fg = "#bbc2cf",
-  yellow = "#ECBE7B",
-  cyan = "#008080",
+  fg = "#CAD3F5",
+  yellow = "#EED49F",
+  cyan = "#8BD5CA",
   darkblue = "#081633",
-  green = "#98be65",
-  orange = "#FF8800",
-  violet = "#a9a1e1",
-  magenta = "#c678dd",
-  purple = "#c678dd",
-  blue = "#51afef",
-  red = "#ec5f67",
+  green = "#A6DA95",
+  orange = "#F5A97F",
+  violet = "#B7BDF8",
+  magenta = "#F5BDE6",
+  purple = "#C6A0F6",
+  blue = "#8AADF4",
+  red = "#ED8796",
 }
 
 return colors
