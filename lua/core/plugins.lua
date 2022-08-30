b5b4006e1436afcc502ee3c0dcee617017589d08
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-08-30 08:41:02 +0200

    Remove markdown plugin
    This was removed due to poor performance.

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -263,11 +263,6 @@ packer.startup({
       end,
     })
     use({
-      "plasticboy/vim-markdown",
-      requires = "godlygeek/tabular",
-      ft = "markdown",
-    })
-    use({
       "iamcco/markdown-preview.nvim",
       run = function()
         vim.fn["mkdp#util#install"]()
