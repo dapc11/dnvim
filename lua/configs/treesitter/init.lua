d89255926e86dba8ed36e8e8f077e3da342d9306
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-09-20 19:54:34 +0200

    Extend treesitter

--- lua/configs/treesitter/init.lua
+++ lua/configs/treesitter/init.lua
@@ -57,8 +57,46 @@ function M.config()
           -- You can use the capture groups defined in textobjects.scm
           ["af"] = "@function.outer",
           ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
+          ["at"] = "@class.outer",
+          ["it"] = "@class.inner",
+          ["ac"] = "@call.outer",
+          ["ic"] = "@call.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+          ["ai"] = "@conditional.outer",
+          ["ii"] = "@conditional.inner",
+          ["a/"] = "@comment.outer",
+          ["i/"] = "@comment.inner",
+          ["ab"] = "@block.outer",
+          ["ib"] = "@block.inner",
+          ["as"] = "@statement.outer",
+          ["is"] = "@scopename.inner",
+          ["aA"] = "@attribute.outer",
+          ["iA"] = "@attribute.inner",
+          ["aF"] = "@frame.outer",
+          ["iF"] = "@frame.inner",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]]"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
         },
       },
       swap = {
