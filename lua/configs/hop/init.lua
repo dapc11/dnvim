0a37aec0626e06ece1b154455c35b755856f2f31
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-09-20 20:01:30 +0200

    Extend hop

--- lua/configs/hop/init.lua
+++ lua/configs/hop/init.lua
@@ -2,10 +2,71 @@ local M = {}
 
 function M.config()
   require("hop").setup({ keys = "asdfgqwertzxcvb" })
-  vim.api.nvim_set_keymap('', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
-  vim.api.nvim_set_keymap('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
-  vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>", {})
-  vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>", {})
+  local opts = { noremap = true, silent = true }
+  local keymap = vim.api.nvim_set_keymap
+
+  keymap("", "L", ":HopWordCurrentLine<cr>", { silent = true })
+  -- keymap("", "S", ":HopChar2<cr>", { silent = true })
+  -- keymap("", "Q", ":HopPattern<cr>", { silent = true })
+  keymap("", "H", ":HopChar2<cr>", { silent = true })
+
+  keymap(
+    "o",
+    "f",
+    ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>"
+    ,
+    opts
+  )
+  keymap(
+    "o",
+    "F",
+    ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>"
+    ,
+    opts
+  )
+  keymap(
+    "o",
+    "t",
+    ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<CR>"
+    ,
+    opts
+  )
+  keymap(
+    "o",
+    "T",
+    ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<CR>"
+    ,
+    opts
+  )
+
+  keymap(
+    "n",
+    "f",
+    ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>"
+    ,
+    opts
+  )
+  keymap(
+    "n",
+    "F",
+    ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>"
+    ,
+    opts
+  )
+  keymap(
+    "n",
+    "t",
+    ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<CR>"
+    ,
+    opts
+  )
+  keymap(
+    "n",
+    "T",
+    ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<CR>"
+    ,
+    opts
+  )
 end
 
 return M
