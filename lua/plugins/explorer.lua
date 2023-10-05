b217628b26dfea2ce8280160e162f6f13716e2b3
Daniel Tingelöf <daniel.tingelof@gmail.com>
2023-10-05 19:55:18 +0200

    Add dot file hide feature to mini.files

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,3 +1,27 @@
+local show_dotfiles = true
+
+local filter_show = function(_)
+  return true
+end
+
+local filter_hide = function(fs_entry)
+  return not vim.startswith(fs_entry.name, ".")
+end
+
+local toggle_dotfiles = function()
+  show_dotfiles = not show_dotfiles
+  local new_filter = show_dotfiles and filter_show or filter_hide
+  require("mini.files").refresh({ content = { filter = new_filter } })
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    vim.keymap.set("n", "<c-h>", toggle_dotfiles, { buffer = buf_id })
+  end,
+})
+
 return {
   {
     "echasnovski/mini.files",
@@ -5,8 +29,10 @@ return {
     keys = {
       {
         "<leader>fe",
-        function()
-          require("mini.files").open()
+        function(_)
+          if not require("mini.files").close() then
+            require("mini.files").open(vim.api.nvim_buf_get_name(0))
+          end
         end,
         desc = "Explorer (cwd)",
       },
