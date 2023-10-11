62ffc8b9af07571fc648685faefb77b3f52d4e86
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-10-11 09:38:21 +0200

    Introduce onedark

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,6 +1,17 @@
 return {
   {
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").setup({
+        style = "dark",
+      })
+      require("onedark").load()
+    end,
+    priority = 1000,
+  },
+  {
     "catppuccin/nvim",
+    enabled = false,
     name = "catppuccin",
     priority = 1000,
     config = function()
