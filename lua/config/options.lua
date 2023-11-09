6437e8f1c878e92525239117a189f2a8ac0e882f
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-11-09 21:24:17 +0100

    Disable statuscolumn calculations in lazyvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -42,6 +42,7 @@ opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
+opt.statuscolumn = "" -- Disable costly statuscolumn calculations
 -- opt.mousescroll = "ver:" .. math.floor(0.5 * vim.o.lines) .. ",hor:6"
 --
 -- if vim.fn.has("nvim-0.10") == 1 then
