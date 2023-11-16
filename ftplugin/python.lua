6d7d7eb7902bdb19c54fd7c1bb0314c0343d5af4
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-11-16 07:19:59 +0100

    Add Python RunCodeQualityChecks command for linting

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -49,3 +49,50 @@ else
     vim.env.PYTHONPATH = nil
   end
 end
+
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set(
+  "n",
+  "ccq",
+  "<cmd>new | 0read !black --config pyproject.toml #<cr>",
+  { desc = "Run Black for Current Buffer", buffer = bufnr }
+)
+vim.keymap.set("n", "ccw", "<cmd>new | 0read !bandit #<cr>", { desc = "Run Bandit for Current Buffer", buffer = bufnr })
+vim.keymap.set(
+  "n",
+  "cce",
+  "<cmd>new | 0read !flake8 --ignore C812,E501 #<cr><cr>",
+  { desc = "Run Flake8 for Current Buffer", buffer = bufnr }
+)
+vim.keymap.set(
+  "n",
+  "ccr",
+  "<cmd>new | 0read !pylint --disable W4901 #<cr><cr>",
+  { desc = "Run Pylint for Current Buffer", buffer = bufnr }
+)
+function RunCodeQualityChecks()
+  local current_file = vim.fn.expand("%:p") -- Get the full path of the current file
+
+  local output_buffer = vim.api.nvim_create_buf(false, true) -- Create a new scratch buffer
+
+  local function run_command(command)
+    local data = vim.fn.system(command .. " " .. current_file .. " 2>/dev/null")
+
+    print("Code quality check (" .. command .. ") executed")
+    if data ~= "" then
+      vim.api.nvim_buf_set_lines(output_buffer, -1, -1, false, { "##############", command, "##############" })
+      vim.api.nvim_buf_set_lines(output_buffer, -1, -1, false, vim.fn.split(data, "\n"))
+    else
+      print("Code quality check (" .. command .. ") passed")
+    end
+  end
+
+  run_command("black --check --config pyproject.toml")
+  run_command("flake8 --ignore C812,E501")
+  run_command("bandit -r")
+  run_command("pylint --disable W4901")
+
+  vim.api.nvim_command("vertical sb " .. vim.fn.bufnr(output_buffer))
+end
+
+vim.api.nvim_set_keymap("n", "<leader>cq", [[:lua RunCodeQualityChecks()<CR>]], { noremap = true, silent = true })
