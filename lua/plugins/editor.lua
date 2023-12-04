9530e529a829f410d2887557d61b793c792d1c6b
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-04 22:40:38 +0100

    feat(editor): add vim-asterisk for productivity

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -45,4 +45,20 @@ return {
       draw = { delay = 500 },
     },
   },
+  {
+    'haya14busa/vim-asterisk',
+    lazy = false,
+    init = function()
+      vim.cmd([[
+        map *   <Plug>(asterisk-*)
+        map #   <Plug>(asterisk-#)
+        map g*  <Plug>(asterisk-g*)
+        map g#  <Plug>(asterisk-g#)
+        map z*  <Plug>(asterisk-z*)
+        map gz* <Plug>(asterisk-gz*)
+        map z#  <Plug>(asterisk-z#)
+        map gz# <Plug>(asterisk-gz#)
+      ]])
+    end
+  }
 }
