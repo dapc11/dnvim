515d966b514612da6af963265033ab5d68a9c6fd
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-04 21:47:01 +0100

    feat(Git): Add blame navigation

--- ftplugin/git.lua
+++ ftplugin/git.lua
@@ -1,11 +1,6 @@
 local bufnr = vim.api.nvim_get_current_buf()
 GetVisualSelection = require("util.common").GetVisualSelection
 
-vim.keymap.set(
-  "n",
-  "gf",
-  function()
-    vim.api.nvim_exec("tab Git show " .. vim.call('expand', '<cword>'), true)
-  end,
-  { desc = "Find Usages", buffer = bufnr }
-)
+vim.keymap.set("n", "gf", function()
+  vim.api.nvim_exec("tab Git show " .. vim.call("expand", "<cword>"), true)
+end, { desc = "Find Usages", buffer = bufnr })
