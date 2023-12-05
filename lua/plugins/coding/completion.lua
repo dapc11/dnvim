79b10de64dbf1bdf71c0e21af0670e03b8258594
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-05 12:21:06 +0100

    fix(completion): Do not lazy load snippets and enable tab completion command mode

--- lua/plugins/coding/completion.lua
+++ lua/plugins/coding/completion.lua
@@ -6,8 +6,10 @@ return {
     "FelipeLema/cmp-async-path",
     {
       "L3MON4D3/LuaSnip",
+      lazy = false,
       dependencies = {
         "rafamadriz/friendly-snippets",
+        lazy = false,
         config = function()
           require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
         end,
@@ -51,12 +53,6 @@ return {
     cmp.setup(opts)
   end,
   opts = function(_, opts)
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     local luasnip = require("luasnip")
     local cmp = require("cmp")
 
@@ -73,7 +69,7 @@ return {
         else
           fallback()
         end
-      end, { "i", "s", "c", }),
+      end, { "i", "s" }),
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
