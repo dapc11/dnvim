0dea8719cf1d8f2255a42d5318f47ea7f9a79494
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-10 21:46:11 +0100

    fix(gopls): setup gopls in more detail

--- lua/plugins/coding/lsp.lua
+++ lua/plugins/coding/lsp.lua
@@ -3,6 +3,44 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = false,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.bob", "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
         typos_lsp = {},
         ruff_lsp = { mason = false, autostart = false },
         lua_ls = {
