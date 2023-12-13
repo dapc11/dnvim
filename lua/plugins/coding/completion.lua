ecad9480a2df182cfeec04a852e257b0e553b29e
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-13 11:42:34 +0100

    fix(completion): Various fixes

--- lua/plugins/coding/completion.lua
+++ lua/plugins/coding/completion.lua
@@ -1,8 +1,15 @@
+local function all_visible_buffers()
+  local bufs = {}
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    bufs[vim.api.nvim_win_get_buf(win)] = true
+  end
+  return vim.tbl_keys(bufs)
+end
+
 return {
   "hrsh7th/nvim-cmp",
   event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
-    "hrsh7th/cmp-cmdline",
     "FelipeLema/cmp-async-path",
     {
       "L3MON4D3/LuaSnip",
@@ -22,52 +29,17 @@ return {
   },
   config = function(_, opts)
     local cmp = require("cmp")
-    local cmd_mapping = vim.tbl_extend("force", cmp.mapping.preset.cmdline(), {
-      ["<C-Space>"] = {
-        c = cmp.mapping.complete(),
-      },
-      ["<Down>"] = {
-        c = function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            fallback()
-          end
-        end,
-      },
-      ["<Up>"] = {
-        c = function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
-        end,
-      },
-    })
 
     for _, source in ipairs(opts.sources) do
       source.group_index = source.group_index or 1
     end
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmd_mapping,
-      sources = {
-        { name = "buffer" },
-      },
-    })
 
     cmp.setup.filetype({ "gitcommit" }, {
       sources = cmp.config.sources({
         {
           name = "buffer",
           option = {
-            get_bufnrs = function()
-              local bufs = {}
-              for _, win in ipairs(vim.api.nvim_list_wins()) do
-                bufs[vim.api.nvim_win_get_buf(win)] = true
-              end
-              return vim.tbl_keys(bufs)
-            end,
+            get_bufnrs = all_visible_buffers,
           },
         },
         { name = "luasnip" },
@@ -85,8 +57,35 @@ return {
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
+    opts.sources = cmp.config.sources({
+      {
+        name = "nvim_lsp",
+        entry_filter = function(entry, _)
+          return require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind()
+            or require("cmp").lsp.CompletionItemKind.Snippet ~= entry:get_kind()
+        end,
+      },
+      { name = "luasnip" },
+      { name = "path" },
+      {
+        name = "buffer",
+        option = {
+          indexing_interval = 1000,
+          keyword_length = 5,
+          get_bufnrs = function()
+            local buf = vim.api.nvim_get_current_buf()
+            local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+            if byte_size > 1024 * 1024 then -- 1 Megabyte max
+              return {}
+            end
+            return all_visible_buffers()
+          end,
+        },
+      },
+    })
+    opts.preselect = cmp.PreselectMode.None
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
