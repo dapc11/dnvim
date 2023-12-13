8866517834da37a13a22fc83a253131dff18c670
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-13 10:05:50 +0100

    chore(cleanup): out commented code

--- ftplugin/blame.lua
+++ ftplugin/blame.lua
@@ -4,4 +4,3 @@ GetVisualSelection = require("util.common").GetVisualSelection
 vim.keymap.set("n", "gf", function()
   vim.api.nvim_exec("tab Git show " .. vim.call("expand", "<cword>"), true)
 end, { desc = "Find Usages", buffer = bufnr })
--- 'viw<cmd>lua vim.cmd.Git({"show " .. GetVisualSelection() })<cr>',
