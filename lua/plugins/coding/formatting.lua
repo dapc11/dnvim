36ace16b365a4138347da6cde7986d590b0f5bd1
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-13 11:43:36 +0100

    fix(formatting): Remove isort as Python formatter

--- lua/plugins/coding/formatting.lua
+++ lua/plugins/coding/formatting.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { "isort", "black" },
+        python = { "black" },
         go = { "goimports", "gofmt" },
       },
     },
@@ -14,7 +14,6 @@ return {
     opts = {
       ensure_installed = {
         "stylua",
-        "isort",
         "black",
         "goimports",
       },
