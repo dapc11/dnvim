ba2bf4f729c3ce6877c9740cd890c133f7229d14
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-20 13:56:36 +0100

    chore(cleanup): Restructuring and reduce treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,61 +1,33 @@
-local load_textobjects = false
+local function large_file(_, bufnr)
+  return vim.api.nvim_buf_line_count(bufnr) > 5000
+end
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     version = false,
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      "nvim-treesitter/playground",
-      "RRethy/nvim-treesitter-textsubjects",
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        init = function()
-          require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-          load_textobjects = true
-        end,
-      },
-    },
     cmd = { "TSUpdateSync" },
     opts = {
-      textsubjects = {
-        enable = true,
-        prev_selection = ",", -- (Optional) keymap to select the previous selection
-        keymaps = {
-          ["."] = "textsubjects-smart",
-          [";"] = "textsubjects-container-outer",
-          ["i;"] = "textsubjects-container-inner",
-        },
-      },
       highlight = {
-        enable = false,
-        disable = function(_, bufnr)
-          return vim.api.nvim_buf_line_count(bufnr) > 5000
-        end,
+        enable = true,
+        disable = large_file,
       },
       indent = {
         enable = true,
-        disable = function(_, bufnr)
-          return vim.api.nvim_buf_line_count(bufnr) > 2000
-        end,
+        disable = large_file,
       },
       ensure_installed = {
         "bash",
-        "c",
-        "html",
         "json",
         "lua",
-        "luadoc",
         "markdown",
         "markdown_inline",
         "python",
         "query",
-        "regex",
-        "vim",
         "yaml",
       },
       playground = { enable = false },
-      -- tree_docs = { enable = true }, lacks python support
       textobjects = {
         select = {
           disable = { "yaml" },
@@ -83,26 +55,11 @@ return {
           },
           include_surrounding_whitespace = true,
         },
-        disable = function(_, bufnr)
-          return vim.api.nvim_buf_line_count(bufnr) > 5000
-        end,
-      },
-      incremental_selection = {
-        enable = true,
-        disable = function(_, bufnr)
-          return vim.api.nvim_buf_line_count(bufnr) > 5000
-        end,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
+        disable = large_file,
       },
     },
     config = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        ---@type table<string, boolean>
         local added = {}
         opts.ensure_installed = vim.tbl_filter(function(lang)
           if added[lang] then
@@ -125,21 +82,6 @@ return {
       }
 
       require("nvim-treesitter.configs").setup(opts)
-
-      if load_textobjects then
-        -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-        if opts.textobjects then
-          for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-            if opts.textobjects[mod] and opts.textobjects[mod].enable then
-              local Loader = require("lazy.core.loader")
-              Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] = nil
-              local plugin = require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
-              require("lazy.core.loader").source_runtime(plugin.dir, "plugin")
-              break
-            end
-          end
-        end
-      end
     end,
   },
 }
