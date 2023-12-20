be09650697014032e8b912641acdb3244a261432
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-20 13:53:20 +0100

    chore(cleanup): Remove illuminate as no extra config was done

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -11,35 +11,6 @@ return {
     },
   },
   {
-    "RRethy/vim-illuminate",
-    event = "LazyFile",
-    config = function(_, opts)
-      require("illuminate").configure(opts)
-
-      local function map(key, dir, buffer)
-        vim.keymap.set("n", key, function()
-          require("illuminate")["goto_" .. dir .. "_reference"](false)
-        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
-      end
-
-      map("l", "next")
-      map("h", "prev")
-
-      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          local buffer = vim.api.nvim_get_current_buf()
-          map("l", "next", buffer)
-          map("h", "prev", buffer)
-        end,
-      })
-    end,
-    keys = {
-      { "l", desc = "Next Reference" },
-      { "h", desc = "Prev Reference" },
-    },
-  },
-  {
     "haya14busa/vim-asterisk",
     lazy = false,
     init = function()
