6bdcf313a3147b4b06cf0a89435820bc6a59d38c
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-12-20 14:18:42 +0100

    chore(cleanup): Keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,27 +2,25 @@ local map = require("util").map
 
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<CR><esc>", { desc = "Escape and clear hlsearch" })
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
--- map({ "i", "v", "n", "s" }, "<C-a>", "<C-u>", { desc = "Scroll up" })
 map({ "n", "v" }, "ö", "{")
 map({ "n", "v" }, "ä", "}")
 
-map({ "n", "x" }, "gw", "*N", { desc = "Search word under cursor" })
+map("n", "gw", "*N", { desc = "Search word under cursor" })
 
-map("n", "zv", "zMzvzz", { desc = "Close all folds except current one" })
-map("n", "z<Down>", "zcjzOzz", { desc = "Close current fold and open next" })
-map("n", "z<Up>", "zckzOzz", { desc = "Close current fold and open previous" })
+map("x", "<leader>p", [["_dP]])
+map({ "n", "v" }, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace" })
 
 -- save file
 map({ "i", "n" }, "<C-s>", "<cmd>w<CR><esc>", { desc = "Save file" })
 map("n", "W", "<cmd>noautocmd w<CR>")
---
 
--- windows
-map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
-map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
-map("n", "<leader>w-", "<C-W>s", { desc = "Split window below", remap = true })
-map("n", "<leader>w|", "<C-W>v", { desc = "Split window right", remap = true })
 map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
 map("n", "<leader>v", "<C-W>v", { desc = "Split window right", remap = true })
 
@@ -37,8 +35,8 @@ map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- Move Lines
--- map("n", "<S-Down>", "<cmd>m .+1<CR>==", { desc = "Move down" })
--- map("n", "<S-Up>", "<cmd>m .-2<CR>==", { desc = "Move up" })
+map("n", "<S-Down>", "<cmd>m .+1<CR>==", { desc = "Move down" })
+map("n", "<S-Up>", "<cmd>m .-2<CR>==", { desc = "Move up" })
 map("i", "<S-Down>", "<esc><cmd>m .+1<CR>==gi", { desc = "Move down" })
 map("i", "<S-Up>", "<esc><cmd>m .-2<CR>==gi", { desc = "Move up" })
 map("v", "<S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move down" })
@@ -77,22 +75,9 @@ map("n", "<leader>cbu", "<cmd>Dispatch bob/bob -q unit-test<CR>", { desc = "Unit
 map("n", "<leader>cbb", "<cmd>Dispatch bob/bob clean init build<CR>", { desc = "Build" })
 map("n", "<leader>cbp", "<cmd>Dispatch bob/bob -q pre-integration-test<CR>", { desc = "Publish to Sandbox" })
 
-map("n", "§", "@:")
-map("n", "<C-c>", function()
-  vim.api.nvim_command("norm! yiw")
-  vim.fn.setreg("/", vim.fn.getreg("+"))
-  vim.api.nvim_feedkeys("ciw", "n", false)
-end, { desc = "Search & Replace" })
-
-map("v", "<C-s>", [[y/\V<C-R>=escape(@",'/\')<CR><CR>Ncgn]], { desc = "Search & Replace" })
-
-map("n", "<g", function()
-  require("treesitter-context").go_to_context()
-end, { silent = true })
-
 vim.cmd([[
-  xnoremap <leader>cR y<cmd>let @/=substitute(escape(@", '/'), '\n', '\\n', 'g')<CR>gvqi
-  nnoremap ; gn@i
+  vmap <silent> * y:let @/=substitute(escape(@",'.$*[^\/~'),'\n','\\n','g')<CR>n
+  vmap <silent> # y:let @/=substitute(escape(@",'.$*[^\/~'),'\n','\\n','g')<CR>N
   map , @q
 
   nmap <c-c> "+y
@@ -117,4 +102,6 @@ vim.cmd([[
   cnoremap <expr> <down> wildmenumode() ? "\<right>" : "\<down>"
   cnoremap <expr> <left> wildmenumode() ? "\<up>" : "\<left>"
   cnoremap <expr> <right> wildmenumode() ? " \<bs>\<C-Z>" : "\<right>"
+  cnoremap <expr> <Tab>   getcmdtype() =~ '[\/?]' ? "<C-g>" : "<C-z>"
+  cnoremap <expr> <S-Tab> getcmdtype() =~ '[\/?]' ? "<C-t>" : "<S-Tab>"
 ]])
