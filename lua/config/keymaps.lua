6a3623c3e98bdc10da067bb6a9c1c2e9ad335726
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-01-09 07:23:26 +0100

    feat(keymaps): use emacs-style keymaps in commandmode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -116,6 +116,24 @@ vim.cmd([[
   cnoremap <expr> <right> wildmenumode() ? " \<bs>\<C-Z>" : "\<right>"
   cnoremap <expr> <Tab>   getcmdtype() =~ '[\/?]' ? "<C-g>" : "<C-z>"
   cnoremap <expr> <S-Tab> getcmdtype() =~ '[\/?]' ? "<C-t>" : "<S-Tab>"
+  " start of line
+  cnoremap <C-A>  <Home>
+  " back one character
+  cnoremap <C-B>  <Left>
+  " delete character under cursor
+  cnoremap <C-D>  <Del>
+  " end of line
+  cnoremap <C-E>  <End>
+  " forward one character
+  cnoremap <C-F>  <Right>
+  " recall newer command-line
+  cnoremap <C-N>  <Down>
+  " recall previous (older) command-line
+  cnoremap <C-P>  <Up>
+  " back one word
+  cnoremap <Esc><C-B> <S-Left>
+  " forward one word
+  cnoremap <Esc><C-F> <S-Right>
 ]])
 
 map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix item" })
