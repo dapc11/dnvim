636e5a215071be85f6a3a0b9c43d07d5e7caa8f4
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-01-14 21:23:26 +0100

    fix(neovide): fonts

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,5 +1,6 @@
-vim.o.guifont = "Liga SFMono Nerd Font:h12"
+vim.o.guifont = "JetBrains Mono:h12,JetBrains Mono:h12,Liga SFMono Nerd Font:h12,SauceCodePro Nerd Font:h12"
 vim.g.neovide_scale_factor = 0.6
 vim.g.neovide_confirm_quit = true
 vim.g.neovide_cursor_trail_size = 0
 vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_scroll_animation_length = 0
