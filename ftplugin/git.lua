3a75b66e6e186d8b19d4dee5a4d3905095fe768c
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-01-26 11:18:54 +0100

    feat(git): Add feature to open Jira tickets from Git commits

--- ftplugin/git.lua
+++ ftplugin/git.lua
@@ -1,6 +1,16 @@
 local bufnr = vim.api.nvim_get_current_buf()
 GetVisualSelection = require("util.common").GetVisualSelection
 
+local function open_jira_in_browser()
+  local jira_id = string.match(vim.fn.getline("."), require("secret").jira_pattern)
+  if jira_id then
+    vim.fn.jobstart({ "xdg-open", require("secret").jira_url .. jira_id }, { detach = true })
+  else
+    print("No Jira ID found in the current line.")
+  end
+end
+
+vim.keymap.set("n", "gj", open_jira_in_browser, { desc = "Find Usages", buffer = bufnr })
 vim.keymap.set("n", "gf", function()
   vim.api.nvim_exec("tab Git show " .. vim.call("expand", "<cword>"), true)
 end, { desc = "Find Usages", buffer = bufnr })
