0b1920cb07ee1f302739fbd4a79c656c5aa43cfd
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-03-10 20:06:49 +0100

    chore(cleanup): flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,14 +2,24 @@ return {
   "folke/flash.nvim",
   event = lazyfile,
   opts = {
-    modes = { search = { enabled = true }, char = { enabled = false } },
+    modes = { char = { enabled = false } },
   },
-  -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump({ search = { forward = true, multi_window = false, wrap = false } })
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").jump({ search = { forward = false, multi_window = false, wrap = false } })
+      end,
+      desc = "Flash Backwards",
+    },
   },
 }
