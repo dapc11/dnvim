bb5f9316e8586fa9df8c2adfc6ebf7450ba85e64
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-05-07 07:45:23 +0200

    feat(vim): add dump capability

--- init.lua
+++ init.lua
@@ -6,6 +6,22 @@ _G.bt = function(...)
 end
 
 vim.print = _G.dd
+vim.dump = function(tbl, indent)
+  if not indent then
+    indent = 2
+  end
+  for k, v in pairs(tbl) do
+    local formatting = string.rep("  ", indent) .. k .. ": "
+    if type(v) == "table" then
+      print(formatting)
+      vim.dump(v, indent + 1)
+    elseif type(v) == "boolean" then
+      print(formatting .. tostring(v))
+    else
+      print(formatting .. v)
+    end
+  end
+end
 _G.lazyfile = { "BufReadPost", "BufNewFile", "BufWritePre" }
 
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
