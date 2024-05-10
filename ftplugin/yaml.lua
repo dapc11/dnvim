aaff76309f2085383d12cca07680fd2776327e69
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-05-10 20:13:33 +0200

    feat(fzf): Add YAML navigation

--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -1,31 +1,31 @@
 local bufnr = vim.api.nvim_get_current_buf()
-local split = require("util.common").split
+
+local function split(str, delimiter)
+  local parts = {}
+  for part in string.gmatch(str, "[^" .. delimiter .. "]+") do
+    table.insert(parts, part)
+  end
+  return parts
+end
+
 local function fzf_yaml()
   require("fzf-lua").fzf_exec('yq e \'.. | select(. == "*") | {(path | join(".")): .}\' ' .. vim.fn.expand("%"), {
     actions = {
       ["default"] = function(selected, _)
-        local parts = {}
-        for part in string.gmatch(selected[1], "[^.]+") do
-          table.insert(parts, part)
-        end
+        local parts = split(selected[1], ".")
         -- Jump to parent of leaf node to make next search accurate
         vim.fn.search(parts[#parts - 1])
         -- Find leaf node name from selected entry
         vim.fn.search(parts[#parts])
       end,
       ["ctrl-k"] = function(selected, _)
-        for _, v in pairs(selected) do
-          vim.fn.setreg("+", split(v, ":")[1])
-          break
-        end
+        vim.fn.setreg("+", split(selected[1], ":")[1])
       end,
       ["ctrl-v"] = function(selected, _)
         local value = ""
-        for _, v in pairs(selected) do
-          for i, x in pairs(split(v, ":")) do
-            if i ~= 1 then
-              value = value .. x
-            end
+        for i, x in pairs(split(selected[1], ":")) do
+          if i ~= 1 then
+            value = value .. x
           end
         end
         vim.fn.setreg("+", value)
