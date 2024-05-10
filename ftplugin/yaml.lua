09fd6dc10af9fc6527d81ff66e40502851acc795
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-05-10 20:13:33 +0200

    feat(fzf): Add YAML navigation

--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -3,6 +3,16 @@ local split = require("util.common").split
 local function fzf_yaml()
   require("fzf-lua").fzf_exec('yq e \'.. | select(. == "*") | {(path | join(".")): .}\' ' .. vim.fn.expand("%"), {
     actions = {
+      ["default"] = function(selected, _)
+        local parts = {}
+        for part in string.gmatch(selected[1], "[^.]+") do
+          table.insert(parts, part)
+        end
+        -- Jump to parent of leaf node to make next search accurate
+        vim.fn.search(parts[#parts - 1])
+        -- Find leaf node name from selected entry
+        vim.fn.search(parts[#parts])
+      end,
       ["ctrl-k"] = function(selected, _)
         for _, v in pairs(selected) do
           vim.fn.setreg("+", split(v, ":")[1])
