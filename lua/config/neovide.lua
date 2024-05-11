fa797a4c6af6c9a253e5e6896f63b5751b415820
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-05-11 22:08:19 +0200

    Neovide tweaks

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,6 +1,6 @@
-vim.o.guifont = "JetBrains Mono:h20"
-vim.g.neovide_scale_factor = 0.6
+vim.o.guifont = "JetBrains Mono:h11"
+vim.g.neovide_scale_factor = 1
 vim.g.neovide_confirm_quit = true
 vim.g.neovide_cursor_trail_size = 0
 vim.g.neovide_cursor_animation_length = 0.1
-vim.g.neovide_scroll_animation_length = 0.1
+vim.g.neovide_scroll_animation_length = 0.2
