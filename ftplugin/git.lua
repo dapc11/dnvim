be93b545fe397abb4581500d82687eea72aeddd5
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-05-11 22:08:49 +0200

    Inline Jira goto in Git commits

--- ftplugin/git.lua
+++ ftplugin/git.lua
@@ -1,12 +1,10 @@
 local bufnr = vim.api.nvim_get_current_buf()
 
-local function open_jira_in_browser()
+vim.keymap.set("n", "gd", function()
   local jira_id = string.match(vim.fn.getline("."), require("secret").jira_pattern)
   if jira_id then
     vim.fn.jobstart({ "xdg-open", require("secret").jira_url .. jira_id }, { detach = true })
   else
     print("No Jira ID found in the current line.")
   end
-end
-
-vim.keymap.set("n", "gd", open_jira_in_browser, { desc = "Goto Jira Definition", buffer = bufnr })
+end, { desc = "Goto Jira Definition", buffer = bufnr })
