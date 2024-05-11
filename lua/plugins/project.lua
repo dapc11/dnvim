671f71a34dc7a0351b18decb6002373feb943702
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-05-11 22:08:31 +0200

    Project tweaks

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,6 +1,10 @@
 return {
   "ahmedkhalf/project.nvim",
-  config = function()
-    require("project_nvim").setup()
+  opts = {
+    detection_mehods = { "pattern", "lsp" },
+    patterns = { ".git", ".stylua.toml", ".gitignore", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+  },
+  config = function(opts)
+    require("project_nvim").setup(opts)
   end,
 }
