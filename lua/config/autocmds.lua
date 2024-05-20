92603e086176660c5be02688249802f36b66e482
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-05-20 09:16:05 +0200

    fix(aucmd): quick close when last buffer

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,23 +1,8 @@
-local count_bufs_by_type = function(loaded_only)
-  loaded_only = (loaded_only == nil and true or loaded_only)
-  local count = { normal = 0, acwrite = 0, help = 0, nofile = 0, nowrite = 0, quickfix = 0, terminal = 0, prompt = 0 }
-  local buftypes = vim.api.nvim_list_bufs()
-  for _, bufname in pairs(buftypes) do
-    if (not loaded_only) or vim.api.nvim_buf_is_loaded(bufname) then
-      local buftype = vim.api.nvim_buf_get_option(bufname, "buftype")
-      buftype = buftype ~= "" and buftype or "normal"
-      count[buftype] = count[buftype] + 1
-    end
-  end
-  return count
-end
-
 local function close_buffer()
-  local bufTable = count_bufs_by_type()
-  if bufTable.normal <= 1 then
-    vim.cmd.close()
+  if vim.fn.winnr("$") == 1 then
+    require("mini.starter").open()
   else
-    vim.cmd.bdelete()
+    vim.cmd.close()
   end
 end
 
