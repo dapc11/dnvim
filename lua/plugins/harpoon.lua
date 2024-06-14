98f8b5a3f0e1407aa1fd7d90ba35f9cdc60324f8
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2024-06-14 21:45:41 +0200

    chore(lcm): plugins

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,24 +1,5 @@
 return {
   {
-    "ThePrimeagen/harpoon",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    -- stylua: ignore
-    keys = {
-      -- { "<leader><Right>", function() require("harpoon.ui").nav_next() end, desc = "Harpoon Next" },
-      -- { "<leader><Left>", function() require("harpoon.ui").nav_prev() end, desc = "Harpoon Previous" },
-      -- { "<leader><Up>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon Menu" },
-      -- { "<leader>m", function() require("harpoon.mark").add_file() end, desc = "Harpoon Add" },
-      -- { "<leader>1", function() require("harpoon.ui").nav_file(1) end, desc = "Harpoon 1" },
-      -- { "<leader>2", function() require("harpoon.ui").nav_file(2) end, desc = "Harpoon 2" },
-      -- { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "Harpoon 3" },
-      -- { "<leader>4", function() require("harpoon.ui").nav_file(4) end, desc = "Harpoon 4" },
-      -- { "<leader>5", function() require("harpoon.ui").nav_file(5) end, desc = "Harpoon 5" },
-      -- { "<leader>6", function() require("harpoon.ui").nav_file(6) end, desc = "Harpoon 6" },
-    },
-  },
-  {
     "cbochs/grapple.nvim",
     opts = {
       scope = "global", -- also try out "git_branch"
