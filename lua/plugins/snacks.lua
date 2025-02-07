5319e965a34feeb94eadeb830856031d06bb0707
Daniel Tingelöf <daniel.tingelof@gmail.com>
2025-02-07 20:46:51 +0100

    Remove clashing section from dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,20 +13,6 @@ return {
           { section = "keys", gap = 1, padding = 1 },
           { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
           { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-          {
-            pane = 2,
-            icon = " ",
-            title = "Git Status",
-            section = "terminal",
-            enabled = function()
-              return Snacks.git.get_root() ~= nil
-            end,
-            cmd = "git status --short --branch --renames",
-            height = 5,
-            padding = 1,
-            ttl = 5 * 60,
-            indent = 3,
-          },
           { section = "startup" },
         },
       },
@@ -126,7 +112,7 @@ return {
       desc = "Find Untracked Git Files",
     },
     {
-      "<C-p>",
+      "<leader>sp",
       function()
         Snacks.picker.projects()
       end,
