3503ef35d34f6c4633092954f4423903d892f8ad
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2025-02-18 12:27:22 +0100

    Add jira_finder to yaml as well as git commit msg

--- lua/util/common.lua
+++ lua/util/common.lua
@@ -67,4 +67,15 @@ function M.split(string, delimiter)
   return Table
 end
 
+
+function M.jira_finder()
+  local jira_id = string.match(vim.fn.getline("."), require("secret").JIRA_PATTERN)
+  if jira_id then
+    print(jira_id)
+    vim.fn.jobstart({ "xdg-open", require("secret").JIRA_URL .. jira_id }, { detach = true })
+  else
+    print("No Jira ID found in the current line.")
+  end
+end
+
 return M
