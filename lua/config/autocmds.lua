007b2c951b156b5f0e78ce4b463f3a508458aa4e
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2025-03-05 08:07:04 +0100

    Keep cursor position when yank

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -162,3 +162,22 @@ endfunction
 "-range=% default is whole file
 command! -range=% GremoveConflictMarkers <line1>,<line2>call RemoveConflictMarkers()
 ]])
+
+
+local cursorPreYank
+vim.keymap.set({ "n", "x" }, "y", function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return "y"
+end, { expr = true })
+vim.keymap.set("n", "Y", function()
+  cursorPreYank = vim.api.nvim_win_get_cursor(0)
+  return "y$"
+end, { expr = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    if vim.v.event.operator == "y" and cursorPreYank then
+      vim.api.nvim_win_set_cursor(0, cursorPreYank)
+    end
+  end,
+})
