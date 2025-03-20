17b50003348ce48fca80e38b5d4c373192461b7f
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2025-03-20 15:09:11 +0100

    Stripped away more features when big buffers to optimize performance

--- lua/plugins/performance.lua
+++ lua/plugins/performance.lua
@@ -1,41 +1,40 @@
 local buf_large_lsp = vim.api.nvim_create_augroup("buf_large", { clear = true })
-local buf_large_common = vim.api.nvim_create_augroup("buf_large_file", { clear = true })
 
-vim.api.nvim_create_autocmd({ "LspAttach", "BufReadPost" }, {
+vim.api.nvim_create_autocmd({ "BufReadPre", "LspAttach" }, {
   callback = function(_)
     local bufnr = vim.api.nvim_get_current_buf()
     local buf_name = vim.api.nvim_buf_get_name(bufnr)
     local ok, stats = pcall(vim.loop.fs_stat, buf_name)
 
     if ok and stats and (stats.size > 400000) then
-      print("Buffer " .. buf_name .. " too big, disabling LSP and Diagnostics...")
+      print("Buffer " .. buf_name .. " too big, disabling features for performance...")
       -- local client = vim.lsp.get_client_by_id(event.data.client_id)
       -- client.server_capabilities.semanticTokensProvider = nil
       vim.lsp.stop_client(vim.lsp.get_clients({ bufnr = bufnr }))
       pcall(vim.diagnostic.enable, false, bufnr)
-    end
-  end,
-  group = buf_large_lsp,
-})
-
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufEnter" }, {
-  callback = function(event)
-    local buf_name = vim.api.nvim_buf_get_name(event.buf)
-    local ok, stats = pcall(vim.loop.fs_stat, buf_name)
-
-    if ok and stats and (stats.size > 10485760) then
-      print("Buffer " .. buf_name .. " too big, performance focus!")
+      vim.b.ministatusline_disable = true
+      vim.b.miniindentscope_disable = true
+      vim.b.loaded_matchparen = 1
+      vim.b.completion = false
       vim.opt_local.spell = false
       vim.opt_local.swapfile = false
       vim.opt_local.bufhidden = "unload"
       vim.opt_local.buftype = "nowrite"
       vim.opt_local.undolevels = -1
-      vim.cmd([[set eventignore+=FileType]])
-    else
-      vim.cmd([[set eventignore-=FileType]])
+      vim.cmd([[
+      autocmd WinEnter <buffer> set eventignore+=CursorHoldI,CursorMovedI,CursorMoved,FileType
+      autocmd WinLeave <buffer> set eventignore-=CursorHoldI,CursorMovedI,CursorMoved,FileType
+      autocmd! * <buffer>
+      set nocursorcolumn nocursorline
+      set nofoldenable
+      set conceallevel=0
+      set updatetime=100
+      syntax off
+      set lazyredraw
+      ]])
     end
   end,
-  group = buf_large_common,
+  group = buf_large_lsp,
 })
 
 return {
