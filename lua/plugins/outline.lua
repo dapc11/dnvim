b0550b6e92f4684ef33f6f2c72f74c7f296c24d4
Daniel Tingelöf <daniel.tingelof@gmail.com>
2025-07-28 21:36:05 +0200

    Introduce outline plugin

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,9 @@
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = {
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {},
+}
