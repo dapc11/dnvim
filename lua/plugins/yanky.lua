a6c7a5f7ef08527780ca7809dbd079eef64be1ab
Daniel Tingelöf <daniel.tingelof@gmail.com>
2025-07-29 21:05:24 +0200

    Reduce yanky configuration

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -15,9 +15,5 @@ return {
       desc = "Open Yank History",
     },
     { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
-    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
-    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
-    { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
-    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
   },
 }
