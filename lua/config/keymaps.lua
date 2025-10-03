7b3c55a2cbba582ee3cebb647e50e2bda9e0e49b
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2025-10-03 22:19:59 +0200

    Add Trim function

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -134,6 +134,11 @@ vim.api.nvim_create_user_command("Dump", function(x)
   vim.api.nvim_put(vim.split(output, "\n"), "l", true, true)
 end, { nargs = "+", desc = "Dump output of a command at the cursor position" })
 
+vim.api.nvim_create_user_command("Trim", function()
+  -- Remove trailing whitespace
+  vim.cmd([[%s/\s\+$//e]])
+end, { desc = "Trim trailing whitespace and ensure single blank line at end" })
+
 map("n", "gV", "`[v`]", { desc = "Select last paste" })
 
 vim.keymap.set("n", "dd", function()
