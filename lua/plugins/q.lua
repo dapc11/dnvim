c516e0d3f0f0aa9cf56eadeb2be09dd42e97fa15
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2025-11-10 09:18:10 +0100

    Disable Q

--- lua/plugins/q.lua
+++ lua/plugins/q.lua
@@ -1,6 +1,7 @@
 return {
   {
     name = "amazonq",
+    enabled = false,
     url = "https://github.com/awslabs/amazonq.nvim.git",
     opts = {
       ssoStartUrl = vim.env.Q_NVIM_IDENTITY_PROVIDER,
