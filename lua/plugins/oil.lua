dad58d8d244642b491bf7bfaf1d3f2f72caefb0c
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2025-12-22 19:17:05 +0100

    Updates to oil,
    - sort capability
    - more detials in list with gd

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -19,7 +19,7 @@ return {
         last_dir = current_dir
       end
     end
-    vim.api.nvim_create_autocmd({"BufEnter", "CursorMoved"}, {
+    vim.api.nvim_create_autocmd({ "BufEnter", "CursorMoved" }, {
       pattern = "oil://*",
       callback = update_cwd,
     })
@@ -35,11 +35,19 @@ return {
       conceallevel = 3,
       concealcursor = "nvic",
     },
+    view_options = {
+      sort = {
+        { "type", "asc" },
+        { "name", "asc" },
+      },
+    },
     prompt_save_on_select_new_entry = true,
     skip_confirm_for_simple_edits = true,
     cleanup_delay_ms = 2000,
     keymaps = {
-      ["<C-s>"] = "actions.select_vsplit",
+      ["<C-s>"] = function()
+        require("oil").set_sort({ { "type", "asc" }, { "mtime", "desc" } })
+      end,
       ["<C-v>"] = "actions.select_split",
       ["<C-t>"] = "actions.select_tab",
       --      ["<C-p>"] = "actions.preview",
@@ -62,6 +70,9 @@ return {
       ["gs"] = "actions.change_sort",
       ["H"] = "actions.toggle_hidden",
       ["<C-h>"] = "actions.toggle_hidden",
+      ["gd"] = function()
+        require("oil").set_columns({ "permissions", "size", "mtime" })
+      end,
       ["g\\"] = "actions.toggle_trash",
       ["<C-g>"] = function()
         require("fzf-lua").live_grep({ cwd = require("oil").get_current_dir() })
@@ -73,4 +84,3 @@ return {
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
-
